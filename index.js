e8bddb6353b48a951e188772231037026785a91f
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-04-04 16:49:29 -0400

    Working fetch of resource list.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+const winston               = require('winston');
+
+const GithubResourceSource = require('./lib/acquisition/github-resource-source');
+
+
+// This should be based on a config really...
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+})
+
+// We will probably need to authenticate to get around the rate limits
+// they are based on IP address, which for us *could* be a major limiter.
+
+
+//https://octokit.github.io/rest.js/#api-Repos-getContent
+
+// Map Content
+
+// Store Content
+
+async function main() {
+
+
+
+    const source = await GithubResourceSource.GetInstance(logger, {
+        repo: 'https://github.com/bryanpizzillo/r4rcontent'
+    });
+ 
+    await source.getResources();
+}
+
+main();
